d944e951429e25c1de103f90d4ab1046eaf5efe5
Ryan Mehta <ryan.mehta@gmail.com>
2018-11-20 13:38:45 -0800

    init

---
+++ week09_redux/class28_redux-review/README.md
@@ -0,0 +1,5 @@
+# Redux Review
+
+## Agenda
+
+* 
---
+++ week09_redux/class28_redux-review/src/components/app/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch, Route
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Fragment>
+      <h1>Dry Cleaner</h1>
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+}
